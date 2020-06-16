0bc2a93d1462f22601f8efa27ade9f8015c09cbf
aplyd <austin@ftacnik.com>
2020-06-16 11:55:14 -0700

    add firebase admin sdk to add firestore

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,5 @@
 const functions = require('firebase-functions');
-const firebase = require('../src/firebase');
+const admin = require('firebase-admin');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
