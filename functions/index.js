713378a98f09dd87c84558497fa3c2874eb222bf
aplyd <austin@ftacnik.com>
2020-06-16 16:37:17 -0700

    get cloud functions working

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,27 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
+admin.initializeApp();
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
+exports.helloWorld = functions.https.onRequest((request, response) => {
+	response.send('Hello from Firebase!');
+});
+
+// exports.confirmUserCreation = functions.auth.user().onCreate((user) => {
+// 	response.send('working');
 // });
 
-//create user
-module.exports = functions.auth.user().onCreate(() => {
-	console.log('working');
-});
+// create user on auth user creation
+// module.exports = functions.auth.user().onCreate(() => {
+// 	console.log('working');
+// 	return null;
+// });
+
+// test
+// module.exports = functions.https.onRequest((request, response) => {
+// 	const number = Math.floor(Math.random() * 100);
+// 	response.send(number.toString());
+// });
