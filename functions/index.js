c82f0fab0bf703888ddf65daf15fbd934827ccf4
aplyd <austin@ftacnik.com>
2020-06-16 20:10:22 -0700

    create user object on anonymous auth

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const { v4: uuidv4 } = require('uuid');
 
 admin.initializeApp();
 
@@ -10,9 +11,19 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 	response.send('Hello from Firebase!');
 });
 
-// exports.confirmUserCreation = functions.auth.user().onCreate((user) => {
-// 	response.send('working');
-// });
+exports.createUser = functions.auth.user().onCreate((user) => {
+	console.log('creating user doc on auth');
+	return admin
+		.firestore()
+		.collection('users')
+		.doc(user.uid)
+		.set({
+			uid: user.uid,
+			imageOne: { start: null, finish: null },
+			imageTwo: { start: null, finish: null },
+			imageThree: { start: null, finish: null },
+		});
+});
 
 // create user on auth user creation
 // module.exports = functions.auth.user().onCreate(() => {
