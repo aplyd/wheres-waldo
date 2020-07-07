be14378053863c22edaaddf52ae26e74d54edfa8
aplyd <austin@ftacnik.com>
2020-07-07 16:35:52 -0700

    add basename

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<Router>
+		<Router basename="wheres-waldo">
 			<App />
 		</Router>
 	</React.StrictMode>,
